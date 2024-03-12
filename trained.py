36b56f254fcc33fbe76d583c7132db3c0fd982da
SuzumiHino <aleks.mellou@gmail.com>
2024-03-12 18:32:41 +0500

    change trained.py

--- trained.py
+++ trained.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import tanh
 import tensorflow as tf
-from utils import vars
+from utils import Vars
 from utils import forwardpropagetion as forwprop
 from utils import backpropagagtion as backprop
